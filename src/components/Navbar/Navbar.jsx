6f6597ebfc44b8b9c28eb15280a5452e13f06249
CodeLight <codelight2023@gmail.com>
2026-07-04 02:57:29 +0100

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,20 +2,19 @@
 import { LuCloudSun } from "react-icons/lu";
 import "./Navbar.css"
 import useWeather from "../../hook/useWeather"
-import { Link } from "react-router-dom";
 function Navbar() {
     const { country, city, handleCountryChange, handleCityChange, handleSubmit } = useWeather()
   return (
     <>
         <header>
             <nav>
-                <Link to='/'>
+                <a href="/">
                     <div className="logo">
                         {/* <SiAccuweather /> */}
                         <LuCloudSun />
                         <h2>CloudSync</h2>
                     </div>
-                </Link>
+                </a>
                 <form onSubmit={handleSubmit}>
                     <div className="box">
                         <input type="text" value={country} onChange={handleCountryChange} placeholder="Country" />
